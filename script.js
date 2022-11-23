fb695458c3d0ddd7cf37cb0c80900a96cb5b77da
MustardPenguin <andyha234@gmail.com>
2022-11-23 18:13:11 -0500

    Completed tic tac toe for player only

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let tiles = document.querySelectorAll('.tile');
 let playerButton = document.querySelectorAll('.player');
 let playButton = document.querySelector('.play');
+let winnerText = document.querySelector('.winner');
 
 const gameBoard = (function() {
     let _board = [
@@ -18,6 +19,14 @@ const gameBoard = (function() {
         _board.forEach( (element) => { 
             element.forEach( (e, index, array) => { array[index] = ''; } )
          })
+         tiles.forEach( (element) => { element.textContent = "" } );
+    }
+    function declareWinner(check) {
+        let winner = check == null ? "Tie" : check === "X" ? _player1 : _player2;
+        winnerText.textContent = winner === "Tie" ? "Tie!" : capitalizeFirstLetter(winner) + " has won the game!";
+        
+        playButton.style.backgroundColor = 'greenyellow';
+        _playing = false;
     }
     function checkWinner() {
         // Horizontal check
@@ -28,18 +37,45 @@ const gameBoard = (function() {
             for(let j = 0; j < 3; j++) {
                 if(_board[i][j] !== check) { won = false; break; }
             }
-            if(won) { console.log("winner found"); return; }
+            if(won) { declareWinner(check); return; }
         }
 
         // Vertical check
+        for(let i = 0; i < 3; i++) {
+            let check = _board[0][i];
+            let won = true;
+            if(check === '') { continue; }
+            for(let j = 0; j < 3; j++) {
+                if(_board[j][i] !== check) { won = false; break; }
+            }
+            if(won) { declareWinner(check); return; }
+        }
 
         // Diagonal check
+        let check = _board[1][1];
+        let won = check !== '' && ((check === _board[0][0] && check === _board[2][2]) || (check === _board[2][0] && check === _board[0][2]));
+        if(won) { declareWinner(_board[1][1]); return; }
+
+        // Tie check
+        let empty = false
+        for(row of _board) {
+            for(col of row) {
+                if(col === '') {
+                    empty = true;
+                    break;
+                }
+            }
+        }
+        if(!empty) {
+            declareWinner();
+        }
     }
     function startGame(player1, player2) {
         _playing = true;
-        _player1 = player1 + "1";
-        _player2 = player2 + "2";
+        _player1 = player1 + " 1";
+        _player2 = player2 + " 2";
         _currentTurn = _player1;
+        winnerText.textContent = "";
         clearBoard();
         console.log("Starting tic tac toe with " + player1 + " vs " + player2);
 
